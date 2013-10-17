56e052d6dd86d730b30f4ded53875d4aef8e53be
Celsian <celsian@gmail.com>
2013-10-17 14:43:54 -0700

    Updated Title and removed auto-admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user
-      if !current_user.admin
-       current_user.admin = true
-       current_user.save
-     end
-   end
   end
 end
