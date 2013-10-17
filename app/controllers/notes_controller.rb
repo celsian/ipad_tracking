6bb259081b2b47dc9a991c4be82c22514abf2e7f
Celsian <celsian@gmail.com>
2013-10-17 14:32:25 -0700

    Added Destroy and strong param for :user_id

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -18,10 +18,17 @@ class NotesController < ApplicationController
     end
   end
 
+  def destroy
+    session[:return_to] ||= request.referer
+    Note.find(params[:id]).destroy
+    
+    redirect_to session.delete(:return_to), flash: {success: "Note was deleted."}
+  end
+
   private
 
   def note_params
-    params.require(:note).permit(:note, :device_id, :student_id)
+    params.require(:note).permit(:note, :device_id, :student_id, :user_id)
   end
 
   def require_admin
