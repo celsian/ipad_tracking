cf3214606ee1e0d883591cdcae9c8781c2260849
Celsian <celsian@gmail.com>
2013-10-17 14:37:32 -0700

    DO NOT USE
    Auto-Admin updates for device-tracking (NOT DLA-tracking)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      if !current_user.admin
+       current_user.admin = true
+       current_user.save
+     end
+   end
   end
 end
