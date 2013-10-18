f3f59ce13cc69e539a0f1cfada5e6d270ac5811b
Celsian <celsian@gmail.com>
2013-10-18 15:55:23 -0700

    Creating functions for admin tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,25 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.disable_all_admins
+    User.all.map do |user|
+      user.admin = false
+      user.save
+    end
+  end
+
+  def self.update_admins(params, current)
+    User.all.map do |user|
+      if params[user.id.to_s]
+        user.admin = true
+        user.save
+      end
+    end
+
+    user = User.find(current.id)
+    user.admin = true
+    user.save
+  end
+
 end
