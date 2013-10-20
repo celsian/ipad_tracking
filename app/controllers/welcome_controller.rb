6490bd50445cd6fd9c049bc59f84127a5a845e22
Celsian <celsian@gmail.com>
2013-10-20 12:02:07 -0700

    Reverting for Production usage.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,13 @@
 class WelcomeController < ApplicationController
 
   def index
-    if current_user
-      if !current_user.admin
-        user = User.find(current_user.id)
-        user.admin = true
-        user.save
-      end
-    end
+    # if current_user
+    #   if !current_user.admin
+    #     user = User.find(current_user.id)
+    #     user.admin = true
+    #     user.save
+    #   end
+    # end
   end
 
 end
