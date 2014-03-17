7b7bf39411e754ba1b3253d62d63ff29123a8e42
Lucas Paolella <celsian@gmail.com>
2014-03-17 14:42:26 -0700

    Validation for amount numerically

--- app/models/finance.rb
+++ app/models/finance.rb
@@ -2,6 +2,7 @@ class Finance < ActiveRecord::Base
   belongs_to :student
 
   validates_presence_of :note, :amount
+  validates :amount, numericality: true
 
   default_scope order("created_at ASC")
   
