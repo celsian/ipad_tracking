c57b79aa9a73329b9f455689c37cac6341e8228b
Lucas Paolella <celsian@gmail.com>
2014-03-21 10:42:36 -0700

    Color for tracking purposes.

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,4 +6,17 @@ class Note < ActiveRecord::Base
   validates :note, presence: true
 
   default_scope order("created_at DESC")
+
+  def color
+    user_email = User.find(user_id).email
+    if user_email == "algarcia@euhsd.net"
+      "green"
+    elsif user_email == "celsian@gmail.com"
+      "blue"
+    elsif user_email == "lgarduno@euhsd.net"
+      "purple"
+    else
+      "black"
+    end
+  end
 end
