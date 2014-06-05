bf428e1d93ee2baeff2edfaa08a5aa78a3140322
Lucas Paolella <celsian@gmail.com>
2014-06-05 07:44:49 -0700

    Rails adds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321172024) do
+ActiveRecord::Schema.define(version: 20140530034102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140321172024) do
     t.integer  "student_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "checked_in"
   end
 
   add_index "devices", ["student_id"], name: "index_devices_on_student_id", using: :btree
